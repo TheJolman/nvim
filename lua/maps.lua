927ddd4b9fa0d21c7c177d46f0b3f69670bb687f
josh <joshuaholman5@gmail.com>
2023-12-12 21:27:36 -0800

    copilot

--- lua/maps.lua
+++ lua/maps.lua
@@ -15,11 +15,30 @@ map('n', '<leader>b', '<CMD>Neotree buffers toggle<CR>', {silent = true})
 
 -- For Buffers
 map('n', '<leader>l', '<CMD>ls<CR>', {silent = true})
-map('n', '<S-TAB>', '<CMD>bp<CR>', {silent = true})
-map('n', '<TAB>', '<CMD>bn<CR>', {silent = true})
-map('n', '<leader>d', '<CMD>bd', {silent = true})
+map('n', '<leader>p', '<CMD>bp<CR>', {silent = true})
+map('n', '<leader>n', '<CMD>bn<CR>', {silent = true})
+map('n', '<leader>d', '<CMD>bd<CR>', {silent = true})
 
 -- Telescope
 map('n', '<leader>ff', '<CMD>Telescope find_files<CR>', {silent = true})
 map('n', '<leader>fb', '<CMD>Telescope buffers<CR>', {silent = true})
 map('n', '<leader>fh', '<CMD>Telescope help_tags<CR>', {silent = true})
+
+-- Copilot
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
+expr = true,
+replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+
+function ToggleCopilot()
+  if vim.g.copilot_enabled == 1 then
+    vim.g.copilot_enabled = 0
+    vim.cmd('echo "Copilot disabled"')
+  else
+    vim.g.copilot_enabled = 1
+    vim.cmd('echo "Copilot enabled"')
+  end
+end
+
+map('n', '<leader>cc', '<CMD>lua ToggleCopilot()<CR>', {silent = true})
